88d2bae68c8e02f9ddf9e6b9780f7624d73b2090
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-11-20 16:49:25 +0530

    asdgf

--- 2_nd_question/frequency.cpp
+++ 2_nd_question/frequency.cpp
@@ -9,8 +9,21 @@ int main(){
  cin >> str;
 int size = str.size();
 
+int count =1;
+for(int i =0; i<size;i++){
+    for (int j = i+1 ; j <size;j++){
+        if( str[i] == str[j]){
+            count++;
+        }
+       
+        
+    }
+   cout <<  str[i] << " :" << count << endl;
+
+   count = 1;
 
 
+}
 
 
 
